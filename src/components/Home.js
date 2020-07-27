561264a4e6587eabf437d819e1108662db24dd9b
Chacko <chackop@gmail.com>
2020-07-27 11:57:49 +0100

    started with semantic ui styles

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div
+      className="ui raised very padded text container segment"
+      style={{ marginTop: '80px' }}
+    >
+      <h3 className="ui header">Home</h3>
+      <p>Home Page TEXT</p>
+    </div>
+  );
+};
